da7fcbecf778764de25f4ea3b34fdf69e10092da
chan <kychan92@gmail.com>
2015-07-21 14:04:09 +0200

    pushed latest version

--- cas/js/main.js
+++ cas/js/main.js
@@ -1,3 +1,12 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
@@ -7,51 +16,11 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-    
-    $('#ibm-content-nav').css('transition', '.5s');
-    
-    
-    //  fade navigator.
-    $(window).scroll(function(e) {
-      var nav = $('#ibm-content-nav');
-      
-      if ($(window).scrollTop() > 10) {
-        if (nav.hasClass('design-common-tabs-black')) {
-            nav.attr('data-blackOnWhite', true);
-            
-            // temporarily change style.
-            nav.removeClass('design-common-tabs-black').addClass('design-common-tabs-white');
-        }
-        
-        //  switch state.
-        nav.css('padding', 0).css('background', '#000');
-      }else{
-        //  reset states.
-        nav.css('padding', '').css('background', '');
-        
-        if (nav.attr('data-blackOnWhite')) {
-            // temporarily change style.
-            nav.removeClass('design-common-tabs-white').addClass('design-common-tabs-black');
-        }
-      }
-    });
-    
-    
-    //  handling popups
-    $('.popup-actvate').click(function() {
-      var itemId  = $(this).attr('href'),
-          item    =  $(itemId)
-      ;
-      
-      if (item.hasClass('hide')) {
-        item.removeClass('hide');
-      }else{
-        item.addClass('hide');
-      }
-    });
 });
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
